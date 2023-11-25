65015a3ad97ad8cac83f64322f0a9be6481be02a
saifmessaoudi <saifmessaoudi99@gmail.com>
2023-11-26 00:56:11 +0100

    setup project

--- app/src/main/java/com/example/moviedex/ui/theme/Color.kt
+++ app/src/main/java/com/example/moviedex/ui/theme/Color.kt
@@ -2,10 +2,9 @@ package com.example.moviedex.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val LightBlue = Color(0xFFBBDEFB)
+val DarkBlue = Color(0xFF0D47A1)
+val LightOrange = Color(0xFFFFCC80)
+val DarkOrange = Color(0xFFFF9800)
+val LightGreen = Color(0xFFA5D6A7)
+val DarkGreen = Color(0xFF4CAF50)
